f94f6ccc2a64dfd1b4f66b3007cc35cd256c2e92
Shailendra Nain <shailnain.6@gmail.com>
2025-05-13 21:24:34 -0400

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Shailendra Nain on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
